d8c8ec9bec3ed3107110d99f0c418d729dd59b2e
Laurence Tuchin <laurencetuchin@gmail.com>
2025-02-17 20:58:07 +1100

    update new astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,66 +1,36 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { defineConfig, squooshImageService } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
-import partytown from '@astrojs/partytown';
 import icon from 'astro-icon';
 import compress from 'astro-compress';
-import astrowind from './vendor/integration';
-import { readingTimeRemarkPlugin, responsiveTablesRehypePlugin, lazyImagesRehypePlugin } from './src/utils/frontmatter.mjs';
-import react from "@astrojs/react";
+
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const hasExternalScripts = false;
-const whenExternalScripts = (items = []) => hasExternalScripts ? Array.isArray(items) ? items.map(item => item()) : [items()] : [];
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), sitemap({
-    filter: (page) => !page.includes('/_'),
-    lastmod: new Date(),
-    serialize: (item) => ({
-      ...item,
-      lastmod: item.lastmod.toISOString(),
-    }),
-    customPages: [
-      'https://cpmcalculator.online/sitemap.xml',
-      'https://cpmcalculator.online/sitemap-0.xml'
-    ]
-  }), mdx(), icon({
-    include: {
-      tabler: ['*'],
-      'flat-color-icons': ['template', 'gallery', 'approval', 'document', 'advertising', 'currency-exchange', 'voice-presentation', 'business-contact', 'database']
-    }
-  }), ...whenExternalScripts(() => partytown({
-    config: {
-      forward: ['dataLayer.push']
-    }
-  })), compress({
-    CSS: true,
-    HTML: {
-      'html-minifier-terser': {
-        removeAttributeQuotes: false
+  integrations: [
+    tailwind({
+      applyBaseStyles: false
+    }), 
+    sitemap(), 
+    mdx(),
+    icon({
+      include: {
+        tabler: ['*']
       }
-    },
-    Image: false,
-    JavaScript: true,
-    SVG: false,
-    Logger: 1
-  }), astrowind({
-    config: './src/config.yaml'
-  }), react()],
-  image: {
-    service: squooshImageService(),
-    domains: ['cdn.pixabay.com']
-  },
-  markdown: {
-    remarkPlugins: [readingTimeRemarkPlugin],
-    rehypePlugins: [responsiveTablesRehypePlugin, lazyImagesRehypePlugin]
-  },
+    }),
+    compress({
+      CSS: true,
+      HTML: true,
+      JavaScript: true,
+      Image: false,
+      SVG: false
+    })
+  ],
   vite: {
     resolve: {
       alias: {
